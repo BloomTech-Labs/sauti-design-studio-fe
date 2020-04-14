691e7ef236b19b010b6bbb9b748c4ab86ce1bf3d
Nicole white <you@example.com>
2020-04-14 06:07:59 -1000

    styled home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,93 +1,53 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
-import image1 from "../images/image1.svg"
-import image2 from "../images/image2.svg"
-import image3 from "../images/image3.svg"
 
 import Navbar from "../components/Navbar";
+import HomeVideo from "../components/HomeVideo";
+import ContactUs from "../components/ContactUs";
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <div className="home-header">
-        <div className="header-content">
-          <h1>Design Your Own Text-Based Flow</h1>
+        <div className='header-container'>
+          <div className="header-content">
+            <h1>Design Your Own Text-Based App</h1>
+            <p>Take your ideas where they need to go.</p>
+          </div>  
+          <Link className='signUpLink' to ="/login">Start your app</Link>
         </div>
       </div>
 
       <div className="home-container">
         <div className="home-content">
-          <img src={image1} alt="placeholder alt text" />
+          <p><span>Sauti Studio + Design</span> allows anyone to build their own text-based apps. Promote your business or share information using your own response flow.</p>
         </div>
 
-        <div className="home-content">
+        <div className="mission">
           <h1>
-            Sharing information made simple & easy with Sauti Design Studio
+            Our mission is to empower people to strengthen their communities and quickly communicate their innovations.
           </h1>
         </div>
       </div>
 
-      <div className="home-container">
-        <div className="home-list">
-          <p>No programming background needed</p>
-          <br />
-          <p>Create, Update, save & delete flows</p>
-          <br />
-          <p>Simple & intuitive visual</p>
-          <br />
-          <p>Cloud-based tool</p>
-          <br />
-          <p>Click through mockup view</p>
-          <br />
-          <p>Simple and Easy</p>
-          <br />
-          <p>No Code Required</p>
-        </div>
-
-        <div className="home-content">
-          <img src={image2} alt="placeholder alt text" />
-        </div>
+      <div className="video-container">
+        <HomeVideo/>
       </div>
 
-      <div className="home-content2">
-      <iframe
-            title="demo"
-            width="1024"
-            height="576"
-            src="https://www.youtube.com/embed/nFO9hyGIBrU"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+      <div className="contact">
+        <ContactUs/>
       </div>
 
-      <div className="home-info">
-        <h1>Knowledge is Power - Sharing it is Powerful</h1>
-        <p>
-          Sauti Studio & Design is an online tool that allows people without any
-          programming background to build their own text-based apps.
-        </p>
-        <p>
-          In Sauti Studio & Design, We offer a simple and easy tool for anyone
-          to create their own flow whether to promote a business or share
-          essential information that can support their community. From bus
-          schedules to a church event calendar, the weather or legal
-          information. You can create any text-based app flow with our product.
-        </p>
-        <p>
-          Our mission is to allow anyone to design their own text-based
-          applications, and share Knowledge that will strengthen the community.
-          Providing a simple tool to create (develop and deploy) empowering
-          solutions and quickly communicate ideas to address property.
-        </p>
-      </div>
+      <footer className="footer">
+        <Footer />
+      </footer>
 
-      <div className="home-content2">
-        <img src={image3} alt="placeholder alt text" />
-      </div>
+
     </>
   )
 }
 
-export default Home
+export default Home;
